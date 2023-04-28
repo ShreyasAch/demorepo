a4c1e81ca5d67376e07e357e43a68cfdbefd94ac
ShreyasAch <shreyas@cloudmpower.com>
2023-04-28 10:01:07 +0000

    28 Apr 2023, 15:31

--- pages/TableView.vue
+++ pages/TableView.vue
@@ -2,7 +2,6 @@
     <div>
         <p> This is updated today</p>
         <p> this is shreyas acharya </p>
-        <p> this is a file change</p>
         <p> This is line which is commited from a local machine</p>
     </div>
 </template>
