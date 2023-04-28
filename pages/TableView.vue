cdcc65dc4a2b2f4e49511bb04a971aca9da7324c
ShreyasAch <shreyas@cloudmpower.com>
2023-04-28 12:45:31 +0530

    local commit

--- pages/TableView.vue
+++ pages/TableView.vue
@@ -2,5 +2,6 @@
     <div>
         <p> This is updated today</p>
         <p> this is shreyas acharya </p>
+        <p> This is line which is commited from a local machine</p>
     </div>
 </template>
